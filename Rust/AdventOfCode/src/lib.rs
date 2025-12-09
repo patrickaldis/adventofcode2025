e9ea1308c95ea2b310c763f223b4284c30de8772
Patrick Aldis <patrick.aldis@virginmedia.com>
2025-12-09 09:08:56 +0000

    add combine

--- Rust/AdventOfCode/src/lib.rs
+++ Rust/AdventOfCode/src/lib.rs
@@ -1,19 +1,60 @@
+use combine::parser::char::{digit, letter, newline};
+use combine::stream::{easy, StreamErrorFor};
+use combine::{many1, optional, sep_by1, Parser, Stream};
+use combine::error::StreamError;
+
+#[derive(PartialEq)]
+#[derive(Debug)]
 pub enum Move {
     MoveRight(u16),
     MoveLeft(u16),
 }
 
-pub fn calculate_sum(sequence: Vec<Move>) -> Vec<i32> {
-    let mut acc: i32 = 50;
-    let mut result: Vec<i32> = vec![50];
-    for x in sequence.iter() {
-        acc += match x {
-            Move::MoveRight(r) => -(*r as i32),
-            Move::MoveLeft(l) => *l as i32,
-        };
-        result.push(acc);
-    }
-    result
+fn integer<Input>() -> impl Parser<Input, Output = i32>
+where
+    Input: Stream<Token = char>,
+{
+    many1(digit()).map(|digits: String| digits.parse::<i32>().unwrap())
+}
+
+fn move_parser<Input>() -> impl Parser<Input, Output = Move>
+where
+    Input: Stream<Token = char>,
+    Input::Range: PartialEq,
+{
+    (letter(), integer()).and_then(|(dir, n)| match dir {
+        'R' => Ok(Move::MoveRight(n as u16)),
+        'L' => Ok(Move::MoveLeft(n as u16)),
+        _ => Err(StreamErrorFor::<Input>::expected_static_message(
+            "expected 'R' or 'L'",
+        )),
+    })
+}
+
+fn move_lines_parser<Input>() -> impl Parser<Input, Output = Vec<Move>>
+where
+    Input: Stream<Token = char>,
+    Input::Range: PartialEq,
+{
+    sep_by1(move_parser(), newline()).skip(optional(newline()))
+}
+
+pub fn cum_sum_mod(sequence: Vec<Move>) -> Vec<i32> {
+    cum_sum(sequence).iter().map(|x| x % 100).collect()
+}
+
+pub fn cum_sum(sequence: Vec<Move>) -> Vec<i32> {
+    sequence
+        .iter()
+        .scan(50, |st, x| {
+            let delta = match x {
+                Move::MoveRight(r) => -(*r as i32),
+                Move::MoveLeft(l) => *l as i32,
+            };
+            *st += delta;
+            Some(*st)
+        })
+        .collect()
 }
 
 pub fn count_zeros(sequence: Vec<i32>) -> u16 {
@@ -30,7 +71,7 @@ mod tests {
     fn check_calculate_sum() {
         let test_input: Vec<Move> =
             vec![Move::MoveRight(1), Move::MoveLeft(2), Move::MoveRight(10)];
-        assert_eq!(calculate_sum(test_input), vec![50, 49, 51, 41]);
+        assert_eq!(cum_sum(test_input), vec![50, 49, 51, 41]);
     }
 
     #[test]
@@ -40,8 +81,20 @@ mod tests {
     }
 
     #[test]
-    fn reading_file() {
-        let contents = fs::read_to_string("src/input.txt").expect("expected to read file");
-        // panic!("{contents}")
+    fn check_parse() {
+        let test_input: String= "L1\nR2\n".to_string();
+    
+        let (moves, _rest) = move_lines_parser().parse(test_input.as_str()).unwrap_or_else(|err| panic!["error occured: {err}"]);
+
+        assert_eq!(
+            moves,
+            vec![Move::MoveLeft(1), Move::MoveRight(2)]
+        )
     }
+
+    // #[test]
+    // fn reading_file() {
+    //     let contents = fs::read_to_string("src/input.txt").expect("expected to read file");
+    //     // panic!("{contents}")
+    // }
 }
